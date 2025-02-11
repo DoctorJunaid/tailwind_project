921fd78893ac4a0a4619577cbfa0df10c62ac711
Muhammad Junaid <junaid.aurangzeb1@gmail.com>
2025-02-11 12:25:17 +0500

    style: enhance mobile menu toggle animation and add desktop menu ID

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 function toggleMenu() {
+    
     const menu = document.getElementById('mobileMenu');
-    menu.classList.toggle('translate-x-full');
-    menu.classList.toggle('translate-x-0');
-    gsap.from('nav>ul>li', {
-        y: -30,
-        opacity: 0,
-        duration: .3,
-        ease: 'ease',
-        stagger: 0.2,
-    });
+    const classList = menu.classList;
+    
+    const isOpen = classList.contains('translate-x-full');
+    classList.remove(isOpen ? 'translate-x-full' : 'translate-x-0');
+    classList.add(isOpen ? 'translate-x-0' : 'translate-x-full');
+    
+    const navItems = document.querySelectorAll('nav > ul > li');
+    
+    gsap.killTweensOf(navItems); 
+    
+    gsap.fromTo(navItems, 
+        {
+            y: -30,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 0.3,
+            ease: "power2.out", 
+            stagger: 0.1, 
+            overwrite: true 
+        }
+    );
 }
 // // animation
 gsap.from('#hero_text1,#hero_text2', {
@@ -33,4 +49,12 @@ gsap.from('form', {
      ease: 'ease-in.Out',
     stagger : 0.5,
     
+});
+
+gsap.from('.logo,#desktop>li', {
+    y:-200,
+     opacity: 0,
+     duration: 1.5,
+     ease: 'ease-in.Out',
+    stagger : .3,
 });
